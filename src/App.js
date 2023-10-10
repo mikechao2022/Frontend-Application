4f23a4d0573aa7cc5446edcf588fcf0c287a8a26
mohammad6vakili <mohammad6vakili@gmail.com>
2023-10-10 19:00:06 +0330

    feat(add api request): :sparkles: add app config api request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react"
 import Router from "router/router"
 import { Routes } from "router/routes/index"
+import useApi from "hooks/use_api"
 
 function App() {
+  const { getAppConfigs } = useApi()
+
+  useEffect(() => {
+    getAppConfigs()
+  }, [])
+
   return (
     <div className="w-full h-screen overflow-y-auto">
       <Router Routes={Routes} />
---
+++ src/components/skeleton/index.js
@@ -0,0 +1,6 @@
+import "./skeleton.css"
+
+const Skeleton = ({ style, classes }) => {
+  return <div style={style} className={`skeleton ${classes ? classes : ""}`}></div>
+}
+export default Skeleton
