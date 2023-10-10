cdb46adba6d6acb42de7c15b9e6cdc79461635bb
mohammad6vakili <mohammad6vakili@gmail.com>
2023-10-10 17:58:57 +0330

    feat(add navigation): :sparkles: add navigation and complete layout

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,4 +1,13 @@
+import Header from "components/header"
+import Sidebar from "components/sidebar"
+
 const Layout = ({ children }) => {
-  return <div className="w-full flex flex-col">{children}</div>
+  return (
+    <div className="w-full flex pt-20">
+      <Header />
+      <Sidebar />
+      <div className="w-[calc(100%-16rem)] p-4">{children}</div>
+    </div>
+  )
 }
 export default Layout
---
+++ src/navigation/index.js
@@ -0,0 +1,17 @@
+import { AiOutlineHome } from "react-icons/ai"
+import { LuLayoutDashboard } from "react-icons/lu"
+
+export const NavigationData = [
+  {
+    id: 1,
+    title: "Main Page",
+    path: "/",
+    icon: <AiOutlineHome fontSize={20} />,
+  },
+  {
+    id: 1,
+    title: "Product",
+    path: "/product/view",
+    icon: <LuLayoutDashboard fontSize={20} />,
+  },
+]
